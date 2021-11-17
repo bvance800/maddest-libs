cf5a7de5ebd1b0da66ebf83945d41c50cad25dc2
Brandon Vance <brandonv@qualtrics.com>
2021-11-16 22:18:00 -0700

    mad lib mini project

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 data = {
-    title: 'My Book',
-    showDiv: true
+    madLibIndex: 0,
+    totalMadLibs: 2,
+    title: 'Maddest Libs',
+    isLoading: false,
+    input_fields: [
+        'name',
+        'noun',
+        'verb',
+        'adjective',
+        'adverb'
+    ]
 }
 
 const app = Vue.createApp({
@@ -9,16 +18,56 @@ const app = Vue.createApp({
         return data
     },
     methods:{
-        changeTitle,
-        toggleView
+        generateMadLib,
     }
 })
 
-function changeTitle(title){
-    this.title = title
+function capitalizeFirstLetter(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
 }
-function toggleView(){
-    this.showDiv = !this.showDiv
+
+function generateMadLib(){
+    for(let i = 0; i < data.input_fields.length; i++){
+        let generatedWordID = data.input_fields[i]+"-place";
+        let generatedWordPlaceList = document.getElementsByClassName(generatedWordID);
+        let inputFieldValue = document.getElementById(data.input_fields[i]).value;
+
+        for(let j = 0; j < generatedWordPlaceList.length; j++){
+            if(inputFieldValue){
+                
+                generatedWordPlaceList[j].innerHTML = inputFieldValue;
+            }
+            else {
+                generatedWordPlaceList[j].innerHTML = "____";
+            }
+
+        }
+    }
+
+    showRandomMadLib();
 }
+
+function showRandomMadLib(){
+
+    let madLibList = document.getElementsByClassName("mad-lib");
+
+    
+    // for(let i = 0; i < madLibList.length; i++){
+    //     madLibList[i].classList.add("notActive");
+    // }
+    let previousMadLibIndex = data.madLibIndex;
+    data.madLibIndex++;
+    data.madLibIndex =  data.madLibIndex % data.totalMadLibs;
+    // console.log("Curr: " + data.madLibIndex + " Prev: " + previousMadLibIndex);
+
+    madLibList[data.madLibIndex].classList.remove("notActive");
+    madLibList[data.madLibIndex].classList.add("mad-lib-enter");
+    madLibList[data.madLibIndex].classList.remove("mad-lib-exit");
+
+    madLibList[previousMadLibIndex].classList.add("mad-lib-exit");
+    madLibList[previousMadLibIndex].classList.remove("mad-lib-enter");
+
+}
+
 
 app.mount('#app')
